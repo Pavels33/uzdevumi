2ab3f047065afd0f2f8a206245531d28aa54dd44
Pavels33 <91064220+Pavels33@users.noreply.github.com>
2021-11-09 12:41:01 +0000

    09.11

---
+++ 09.11/programma.py
@@ -0,0 +1,14 @@
+""" Ievietot pilnu failu"""
+# import bibloteka
+
+# robots = bibloteka.Robots("Viens")
+
+# robots.sasviecinaties()
+
+""" Ievietot ar aizstajejvardu"""
+# import bibloteka as bibl
+
+# robots = bibl.Robots("Divi")
+
+# robots.sasviecinaties()
+
